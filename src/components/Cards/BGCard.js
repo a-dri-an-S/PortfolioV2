951e9e9168a7560c59175129b00c5d9d37ec67e2
Adrian S <adrian.salinas.1986@gmail.com>
2022-03-13 20:58:33 -0700

    added BGCard w/ dummy text

--- src/components/Cards/BGCard.js
+++ src/components/Cards/BGCard.js
@@ -1,7 +1,9 @@
 const BGCard = () => {
     return (
-        <div>
-            Background Card
+        <div className="bg-card">
+            <p className="bg-card">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+            </p>
         </div>
     );
 }
