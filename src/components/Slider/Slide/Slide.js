7b8c8c466422ed664e21ab680f7b1dd458c46ca0
Adrian S <adrian.salinas.1986@gmail.com>
2022-03-16 20:56:10 -0700

    flipped content on Slide + updated styles

--- src/components/Slider/Slide/Slide.js
+++ src/components/Slider/Slide/Slide.js
@@ -4,18 +4,18 @@ const Slide = ({ title, img, tech, details, repoUrl, liveUrl }) => {
     return (
         <div className="slide">
             <div className="slide-content">
-                <div className="slide-content-left">
+            <div className="slide-content-left">
+                    <h3 className="slide-title">{title}</h3>
+                    <div className="slide-title-underline">.</div>
+                    {details}
+                </div>
+                <div className="slide-content-right">
                     <div className="slide-img-container">
                         <img className="slide-img" src={img} alt={title} />
                     </div>
                     <div className="slide-info">
                         {tech}
                     </div>
-                </div>
-                <div className="slide-content-right">
-                    <h3 className="slide-title">{title}</h3>
-                    <div className="slide-title-underline">.</div>
-                    {details}
                 </div>
             </div>
             <div className="slide-links">
