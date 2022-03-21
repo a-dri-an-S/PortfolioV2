8cfd1f33feb94f89aba8aec417bb5c7b998107b3
Adrian S <adrian.salinas.1986@gmail.com>
2022-03-21 16:47:55 -0700

    added CSS breakpoint 480px to AboutMe page + components

--- src/components/NavBar/MobileNavBar.js
+++ src/components/NavBar/MobileNavBar.js
@@ -108,8 +108,8 @@ const MobileNavBar = () => {
             </nav>
             <Routes>
                 <Route path="/" element={<Main />} />
-                {/* <Route path="/about-me" element={<AboutMe />} />
-                <Route path="/projects" element={<Projects />} />
+                <Route path="/about-me" element={<AboutMe />} />
+                {/* <Route path="/projects" element={<Projects />} />
                 <Route path="/contact" element={<Contact />} /> */}
             </Routes>
         </section>
