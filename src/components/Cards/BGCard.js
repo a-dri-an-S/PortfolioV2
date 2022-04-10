ddf0f01e29dd67bb36e32c7931b989dda821d421
Adrian Salinas <adrian.salinas.1986@gmail.com>
2022-04-09 20:17:32 -0600

    Added copy to BGCard - AboutMe page

--- src/components/Cards/BGCard.js
+++ src/components/Cards/BGCard.js
@@ -2,7 +2,19 @@ const BGCard = () => {
     return (
         <div className="background">
             <p className="background-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+                I'm a Full-Stack web developer with a new found LOVE in creating responsive and functional websites.
+            </p>
+            <p className="background-content">
+                My background ranges from Produce Manager, Retail, Customer Service and Laser Engraving.
+            </p>
+            <p className="background-content">
+                I've had the pleasure of working for giant organizations like Whole Foods Merket, to smaller, yet impactful companies like OneHope Wine.
+            </p>
+            <p className="background-content">
+                As of late, I have been bit by the coding bug (no debugging required) enojoy solving problems with code.
+            </p>
+            <p className="background-content">
+                Let's make the web an enjoyable place!
             </p>
         </div>
     );
